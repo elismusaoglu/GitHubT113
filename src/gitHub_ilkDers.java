ed9bb226de6c448d3fc2f5090e11bfbecffa2679
elismusaoglu <elis.musaoglu@gmail.com>
2023-05-25 21:59:37 +0100

    230525

--- src/gitHub_ilkDers.java
+++ src/gitHub_ilkDers.java
@@ -1,5 +1,7 @@
 public class gitHub_ilkDers {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+        System.out.println("===========");
+        System.out.println("have a wise dayg");
     }
 }
